252cd0ffa9de614156f2f06e679f2ebc9d1126e7
jotap1101 <jotap1101.joaopedro@gmail.com>
2025-10-01 22:18:33 -0300

    Add additional CORS allowed origins for local development

--- config/settings.py
+++ config/settings.py
@@ -214,6 +214,8 @@ CORS_ALLOWED_ORIGINS = env.list(
     default=[
         "http://localhost:3000",
         "http://127.0.0.1:3000",
+        "http://127.0.0.1:8000",
+        "http://localhost:8000",
     ],
 )
 CORS_ALLOW_CREDENTIALS = True
