3bf8e51faa557a04b91a5844ec424d1cc94dc53c
jotap1101 <jotap1101.joaopedro@gmail.com>
2025-10-01 22:27:00 -0300

    Add pagination settings to REST framework configuration

--- config/settings.py
+++ config/settings.py
@@ -169,6 +169,8 @@ REST_FRAMEWORK = {
     "DEFAULT_VERSION": "v1",
     "ALLOWED_VERSIONS": ["v1"],
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 10,
 }
 
 # JWT settings
